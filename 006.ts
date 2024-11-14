2f1e3c2ce0b1478ddd5ff1b9987a7084f831b4aa
Mayur Panchal <mayur.panchal@deepintent.com>
2024-11-14 22:02:07 +0530

    feat: ✨ implement pattern 6

---
+++ 006.ts
@@ -0,0 +1,31 @@
+/*
+
+* * * *
+ * * *
+  * *
+   *
+
+*/
+import { getUserInput } from '.';
+
+const main = async () => {
+  const n = +await getUserInput("Enter the number: ");
+  
+  // row
+  for(let row = n; row>= 1; row--){
+    let str = "";
+
+    // print spaces 
+    for(let col = 1; col <= n - row; col++){
+      str += " ";
+    }
+
+    // print stars 
+    for(let col = 1; col <= row; col++){
+      str += "* ";
+    }
+    console.log(str);
+  }
+}
+
+main();
