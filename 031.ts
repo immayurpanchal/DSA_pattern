0c7a7f932c5f8d65403dc8a1f50b0e956b4b6d41
Mayur Panchal <mayur.panchal@deepintent.com>
2024-11-19 19:12:05 +0530

    feat: ✨ implement pattern 31

---
+++ 031.ts
@@ -0,0 +1,32 @@
+/*
+
+1
+1 0
+1 0 1
+1 0 1 0
+1 0 1 0 1
+
+*/
+import { getUserInput } from '.';
+
+const main = async () => {
+  const n = +await getUserInput("Enter the number: ");
+
+  // Ascending Pattern
+  for(let row= 1; row <=n; row++){
+    let str = "";
+
+    for(let col = 1; col <= row; col++){
+      if(col % 2 === 1){
+        str += "1 ";
+      }else {
+        str += "0 ";
+      }
+    }
+
+    console.log(str)
+
+  }
+}
+
+main();
