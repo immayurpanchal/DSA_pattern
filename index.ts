a0e2e21b27b0ca2dbf5cd8cb1c4c96b450052cef
Mayur Panchal <mayur.panchal@deepintent.com>
2024-11-20 01:06:10 +0530

    update stdin stream to take multiple inputs

--- index.ts
+++ index.ts
@@ -5,6 +5,6 @@ export const getUserInput = async (promptText = '') => {
   process.stdout.write(promptText); // Display the prompt
   const [data] = await once(stdin, 'data') as Buffer[];
   const userInput = await data.toString().trim();
-  stdin.destroy(); // Close stdin stream
+  // stdin.destroy(); // Close stdin stream
   return userInput;
 }
